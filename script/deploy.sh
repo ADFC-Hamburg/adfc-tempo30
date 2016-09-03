c5720f1a59e1816e21c335640cc3f14ba70ec422
Sven Anders <sven@anders.hamburg>
2016-09-03 13:59:10 +0200

    #3 travis deploy only /dist

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/dist travis@tools.adfc-hamburg.de:/var/www/html/tempo30/$TRAVIS_BRANCH
+rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/dist/ travis@tools.adfc-hamburg.de:/var/www/html/tempo30/$TRAVIS_BRANCH
 ssh travis@tools.adfc-hamburg.de "cd /var/www/html/tempo30/$TRAVIS_BRANCH ; nodejs update-overpass.js"
