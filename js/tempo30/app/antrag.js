44bfdcd473992049e55f40c62a2a40ee57cd4367
Sven Anders <sven@anders.hamburg>
2016-09-11 09:47:25 +0200

    Download des Antrags

--- js/tempo30/app/antrag.js
+++ js/tempo30/app/antrag.js
@@ -6,7 +6,8 @@ define('tempo30/app/antrag', [
     'tempo30/view/str_wahl_dialog',
     'tempo30/view/str_not_found_dialog',
     'tempo30/view/fehler_melden_dialog',
-], function ($, version, step1dialog, step2dialog, step3dialog, strNfDialog, errorDialog) {
+    'tempo30/view/download_dialog',
+], function ($, version, step1dialog, step2dialog, step3dialog, strNfDialog, errorDialog, step4dialog) {
 
     function nominatimSearch(str, nr) {
 	var baseUrl='https://nominatim.openstreetmap.org/search';
@@ -32,15 +33,26 @@ define('tempo30/app/antrag', [
 	    step2dialog(data, step1, step3).open();
 	}).fail(function (e) {
 	    console.error(e);
+	    // FIXME
 	    alert('Fehler');
 	});
     }
     function step3(data) {
-	step3dialog(data, step2, step4, strNotFound).open();
+	$.ajax({
+	    'url': 'https://tools.adfc-hamburg.de/tempo30-backend/geodaten.php?lat='+data.lat+'&lon='+data.lon,
+	    'dataType':'json'
+	}).done( function (geodata) {
+	    data=$.extend(data,geodata);
+	    step3dialog(data, step2, step4, strNotFound).open();
+	}).fail( function (e) {
+	    console.error('error',e);
+	    // FIXME
+	    alert('Fehler');
+
+	});
     }
     function step4(data) {
-	console.log(data);
-	alert('Not implemented yet');
+	step4dialog(data, step3).open();
     }
     function strNotFound(data) {
 	strNfDialog(data, step2, function () {}, errorStrNotFound).open();
@@ -50,4 +62,6 @@ define('tempo30/app/antrag', [
     }
 
     return step1;
+
+//{"str":"Cuxhavener Straße","name":"Max Mustermann","hausnr":"78","plz":"21149","lat":"53.4715402","lon":"9.90065095","antrag":["Waltershofer Straße","Cuxhavener Straße"],polizei:[{"bemerkung":"PK-Grenzen","region":"Harburg","pk":"PK 47","vd":"VD 4","polizeirev":"47","name":"PK47 Neugraben","strasse":"Neugrabener Markt 3","plz":"21149","ort":"Hamburg","tel":"040 428 65-4710"},],ort:[{"bezirk_name":"Harburg","stadtteil":"Hausbruch","ortsteilnummer":"714","bezirk":"7"},],laerm_tag:[{"klasse":"5"},],laerm_nacht:[{"klasse":"5"},],luftdaten:[{"gid":"2183","name_12":"Bundesstrae B73","no2_i1_gb":"30.15055","pm10_i1_gb":"25.4","pm25_i1_gb":"18.7","st_astext":"MULTILINESTRING((9.89456688524801 53.4717257209532,9.89706589630153 53.4716198843808,9.89755441490041 53.4715843238093,9.89812061423444 53.4715334425004,9.89875985895506 53.4714404078619))","st_distance":"4.47805368471465"},]}
 });
