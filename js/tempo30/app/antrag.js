64e6618a9436649e2f878dfbd37c48e80e6ad85f
Sven Anders <sven@anders.hamburg>
2016-10-18 16:06:30 +0200

    Ergebnis-Dialog, Abfrage weiterer Daten, Antrag als Doc, fixes #45, #39 #43 #44 #42

--- js/tempo30/app/antrag.js
+++ js/tempo30/app/antrag.js
@@ -7,8 +7,9 @@ define('tempo30/app/antrag', [
     'tempo30/view/str_not_found_dialog',
     'tempo30/view/fehler_melden_dialog',
     'tempo30/view/download_dialog',
+    'tempo30/app/create-word',
     //
-], function ($, version, step1dialog, step2dialog, step3dialog, strNfDialog, errorDialog, step4dialog) {
+], function ($, version, step1dialog, step2dialog, step3dialog, strNfDialog, errorDialog, step4dialog, createWord) {
 
     function nominatimSearch(str, nr) {
 	var baseUrl='https://nominatim.openstreetmap.org/search';
@@ -21,6 +22,16 @@ define('tempo30/app/antrag', [
 	});
     }
 
+    function start() {
+	step1();
+
+/*	var data={};
+	//data.lat=53.4733698;
+	//data.lon=9.89281277104623;
+	data.lat=53.471342867371405;
+	data.lon=9.86020803451538;
+	step3(data);*/
+    }
     function step1() {
 	step1dialog(step2).open();
     }
@@ -38,11 +49,30 @@ define('tempo30/app/antrag', [
 	    alert('Fehler');
 	});
     }
+
     function step3(data) {
-	step3dialog(data, step2, step4, strNotFound).open();
+	var dialog=step3dialog(data, step2, step4, strNotFound);
+	dialog.open();
+	$.ajax({
+	    'url': 'https://tools.adfc-hamburg.de/tempo30-backend/master/geodaten.php?lat='+data.lat+'&lon='+data.lon,
+	    'dataType':'json'
+	}).done( function (geodata) {
+	    data=$.extend(data,geodata);
+	    dialog.setGeoData(data);
+	}).fail(function (e) {
+	    console.error(e);
+	    dialog.close();
+	    // FIXME
+	    alert('Fehler');
+	});
     }
     function step4(data) {
-	step4dialog(data, step3).open();
+	step4dialog(data, step3, step5).open();
+    }
+    function step5(data) {
+	console.log(data);
+	createWord.download(data);
+	//step5dialog(data, step4).open();
     }
     function strNotFound(data) {
 	strNfDialog(data, step2, function () {}, errorStrNotFound).open();
@@ -51,7 +81,7 @@ define('tempo30/app/antrag', [
 	errorDialog('Tempo 50 Str. nicht gefunden ('+data.lat+' '+data.lon+')','lat='+data.lat+' lon='+data.lon+' version='+version.revision+'('+version.date+')'+"\n"+data.str+data.hausnr).open();
     }
 
-    return step1;
+    return start;
 
 //{"str":"Cuxhavener Straße","name":"Max Mustermann","hausnr":"78","plz":"21149","lat":"53.4715402","lon":"9.90065095","antrag":["Waltershofer Straße","Cuxhavener Straße"],polizei:[{"bemerkung":"PK-Grenzen","region":"Harburg","pk":"PK 47","vd":"VD 4","polizeirev":"47","name":"PK47 Neugraben","strasse":"Neugrabener Markt 3","plz":"21149","ort":"Hamburg","tel":"040 428 65-4710"},],ort:[{"bezirk_name":"Harburg","stadtteil":"Hausbruch","ortsteilnummer":"714","bezirk":"7"},],laerm_tag:[{"klasse":"5"},],laerm_nacht:[{"klasse":"5"},],luftdaten:[{"gid":"2183","name_12":"Bundesstrae B73","no2_i1_gb":"30.15055","pm10_i1_gb":"25.4","pm25_i1_gb":"18.7","st_astext":"MULTILINESTRING((9.89456688524801 53.4717257209532,9.89706589630153 53.4716198843808,9.89755441490041 53.4715843238093,9.89812061423444 53.4715334425004,9.89875985895506 53.4714404078619))","st_distance":"4.47805368471465"},]}
 });
--- js/tempo30/view/download_dialog.js
+++ js/tempo30/view/download_dialog.js
@@ -19,25 +19,46 @@ define('tempo30/view/download_dialog', [
 	    },
 	    {
 		id: 'next-btn',
-		label: gt('beenden'),
+		label: gt('weiter'),
 //		cssClass: 'btn-primary',
 		title: gt('Zeigt den Antragstext in einem neuen Fenster'),
 		action: function (dialogRef) {
+		    data.antrag_str = $.trim(dialogRef.getModalBody().find('#antrag_str').val());
+		    data.name = $.trim(dialogRef.getModalBody().find('#name').val());
+		    data.email = $.trim(dialogRef.getModalBody().find('#email').val());
+		    data.newsletter = dialogRef.getModalBody().find('#newsletter').prop('checked');
+		    data.adfc_contact = dialogRef.getModalBody().find('#adfc').prop('checked');
 		    dialogRef.close();
+		    nextCb(data);
 		}
 	    }];
 	var dialog = new BootstrapDialog({
             'title': gt('Tempo 30 benatragen, Schritt 4: Antrag herunterladen, bearbeiten, unterschreiben und abschicken.'),
-            'message': gt('<ol><li>. Laden Sie unseren Antragsentwurf herunter:')+
-		'<form action="https://tools.adfc-hamburg.de/tempo30-backend/master/antrag.php" method="POST" accept-charset="UTF-8" target="_blank"><input type="hidden" id="data" name="data" value=""/><button type="submit" class="btn btn-primary" id="next-btn">Antrag speichern</button></form></li>'+
-		gt('<li>Bitte bearbeiten Sie den Antrag. Der Antrag ist so NICHT FERTIG, passen Sie ihn bitte an die Situation vor Ort an.</li>')+
-		gt('<li>Unterschreiben -> Abschicken (sollten Sie den Antrag persönlich abgeben, zweimal ausdrucken und den Empfang auf dem einen exemplar bestätigen)</li>')+
-		gt('<li>(Optional) Den Antrag bitte auch an uns schicken</li>')+ //FIXME
-		gt('<li>(Optional) Sich in unserem Newsletter eintragen</li>')+ //FIXME
-		gt('<li>Erzählen Sie es weiter, z.B. auf Twitter oder Facebook</li>'), //FIXME
+            'message': gt('Noch ein Schritt, dann wird ihnen ein Antragsentwurf zu Download angeboten. Bitte ergänzen Sie folgende Daten:')+
+	        '\n\n'+
+	        gt('Straße für die Sie Tempo 30 beantragen (die Straße muss an Ihr Haus angrenzen)')+
+		'<input type="text" id="antrag_str" class="form-control">\n'+
+                gt('Ihr Name (optional):')+
+		'<input type="text" id="name" class="form-control">\n'+
+		'<div class="checkbox"><label><input type="checkbox" id="newsletter" value="">Ich möchte den Newsletter erhalten</label></div>'+
+		'<div class="checkbox"><label><input type="checkbox" id="adfc" value="">Der ADFC darf meine E-Mailaddresse speichern und mich für Rückfragen zum Antrag kontaktieren.</label></div>\n'+
+		gt('Ihre E-Mailaddresse:')+
+		'<input type="email" id="email" class="form-control">'+
+
+		'',
             'buttons': buttons,
 	    onshown: function(dialogRef){
-		dialogRef.getModalBody().find('#data').prop('value', JSON.stringify(data));
+		var body=dialogRef.getModalBody();
+		body.find('#antrag_str').prop('value', data.str);
+		body.find('#email').prop('disabled', true);
+		function changeEmailStatus () {
+		    body.find('#email').prop('disabled',
+					     (!(body.find('#newsletter').prop('checked') ||
+						body.find('#adfc').prop('checked'))));
+		}
+		body.find('#newsletter').change(changeEmailStatus);
+		body.find('#adfc').change(changeEmailStatus);
+
 	    },
 	    onhide: function(dialogRef){
             },
