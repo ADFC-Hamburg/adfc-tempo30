fc954fb1e2bd3e06c446b9836aed88c4614bfc99
Sven Anders <sven@anders.hamburg>
2016-11-13 11:33:33 +0100

    Fehlerbehandlung, fixes #46 and #62

--- js/tempo30/app/antrag.js
+++ js/tempo30/app/antrag.js
@@ -5,12 +5,15 @@ define('tempo30/app/antrag', [
     'tempo30/view/position_dialog',
     'tempo30/view/result_dialog',
     'tempo30/view/fehler_melden_dialog',
+    'tempo30/view/fehler_aufgetaucht_dialog',
     'tempo30/view/download_dialog',
     'tempo30/app/create-word',
     'tempo30/view/wie_geht_es_weiter_dialog',
-    'tempo30/view/bmu_daten_anfrage'
+    'tempo30/view/bmu_daten_anfrage',
+    'tempo30/view/polizeireview_ermitteln_fehler_dialog',
+    'gettext!tempo30'
     //
-], function ($, version, step1dialog, step2dialog, step3dialog, errorDialog, step4dialog, createWord, step5dialog, bmuDatenAnfrage) {
+], function ($, version, step1dialog, step2dialog, step3dialog, errorDialog, errorOccDialog, step4dialog, createWord, step5dialog, bmuDatenAnfrage, polizeiRvErr, gt) {
 
     function nominatimSearch(str, nr) {
 	var baseUrl='https://nominatim.openstreetmap.org/search';
@@ -64,22 +67,15 @@ define('tempo30/app/antrag', [
 	var browser= getBrowser();
 
 	if (browser === 'Safari') {
-	    alert('Es gibt ein Problem mit dem Safari Browser. Bitte benutzen Sie Chrome oder Firefox.');
+	    alert(gt('Es gibt ein Problem mit dem Safari Browser. Bitte benutzen Sie Chrome oder Firefox.'));
 
 	} else if ((browser !== 'Chrome') && (browser !== 'Firefox')) {
-	    alert('Ihr Browser wird evtl. nicht unterstützt. Sicher getestet sind: Firefox, Chrome und Chromium. Testen Sie gerne die Anwendung und geben Sie uns Rückmeldung ob es funktioniert hat. Insbesondere der Download des Word-Dokuments kann evtl. fehlschlagen.');
+	    alert(gt('Ihr Browser wird evtl. nicht unterstützt. Sicher getestet sind: Firefox, Chrome und Chromium. Testen Sie gerne die Anwendung und geben Sie uns Rückmeldung ob es funktioniert hat. Insbesondere der Download des Word-Dokuments kann evtl. fehlschlagen.'));
 
 	    step1();
 	} else {
 	    step1();
 	}
-
-/*	var data={};
-	//data.lat=53.4733698;
-	//data.lon=9.89281277104623;
-	data.lat=53.471342867371405;
-	data.lon=9.86020803451538;
-	step3(data);*/
     }
     function step1() {
 	step1dialog(step2, errorDialog).open();
@@ -93,9 +89,7 @@ define('tempo30/app/antrag', [
 	    data.lon=d[0].lon;
 	    step2dialog(data, step1, step3, errorDialog).open();
 	}).fail(function (e) {
-	    console.error(e);
-	    // FIXME
-	    alert('Fehler');
+            errorOccDialog('Fehler bei der Suche mit Nominatim', JSON.stringify(e)+JSON.stringify(data)).open();
 	});
     }
 
@@ -107,12 +101,23 @@ define('tempo30/app/antrag', [
 	    'dataType':'json'
 	}).done( function (geodata) {
 	    data=$.extend(data,geodata);
+            if ((data.polizei===false) ||
+                (data.polizei.length===0) ||
+                (data.polizei[0].name === undefined) ||
+                (data.polizei[0].strasse === undefined) ||
+                (data.polizei[0].plz === undefined))
+            {
+
+                data.polizei=[{name: gt('POLIZEIREVIER'),
+                               strasse: gt('POLIZEI-STRASSE'),
+                               plz:'2xxx'
+                              }];
+                polizeiRvErr(data, errorDialog).open();
+            } 
 	    dialog.setGeoData(data);
 	}).fail(function (e) {
-	    console.error(e);
-	    dialog.close();
-	    // FIXME
-	    alert('Fehler');
+            dialog.close();
+            errorOccDialog(gt('Fehler bei der Geodatensuche an tools'), JSON.stringify(e)+JSON.stringify(data)).open();
 	});
     }
     function step4(data) {
@@ -160,9 +165,8 @@ define('tempo30/app/antrag', [
 	    sendToAdfc.showInMap = data.adfc_map;
 	    sendToAdfc.noLimit = data.adfc_all;
 	    $.post('https://tools.adfc-hamburg.de/tempo30-backend/master/save.php', sendToAdfc).fail(function (e) {
-		console.error(e);
-		alert('Fehler bei der Datenübertragung zum ADFC');
-		//FIXME bessere Fehlerbehandlung!
+		errorOccDialog('Fehler bei der Datenübertragung an tools', JSON.stringify(e)+JSON.stringify(data)).open();
+
 	    });
 	}
 
@@ -173,5 +177,4 @@ define('tempo30/app/antrag', [
     }
     return start;
 
-//{"str":"Cuxhavener Straße","name":"Max Mustermann","hausnr":"78","plz":"21149","lat":"53.4715402","lon":"9.90065095","antrag":["Waltershofer Straße","Cuxhavener Straße"],polizei:[{"bemerkung":"PK-Grenzen","region":"Harburg","pk":"PK 47","vd":"VD 4","polizeirev":"47","name":"PK47 Neugraben","strasse":"Neugrabener Markt 3","plz":"21149","ort":"Hamburg","tel":"040 428 65-4710"},],ort:[{"bezirk_name":"Harburg","stadtteil":"Hausbruch","ortsteilnummer":"714","bezirk":"7"},],laerm_tag:[{"klasse":"5"},],laerm_nacht:[{"klasse":"5"},],luftdaten:[{"gid":"2183","name_12":"Bundesstrae B73","no2_i1_gb":"30.15055","pm10_i1_gb":"25.4","pm25_i1_gb":"18.7","st_astext":"MULTILINESTRING((9.89456688524801 53.4717257209532,9.89706589630153 53.4716198843808,9.89755441490041 53.4715843238093,9.89812061423444 53.4715334425004,9.89875985895506 53.4714404078619))","st_distance":"4.47805368471465"},]}
 });
