3f24290da339c1a6403a718e097b4dea4fc689ed
Sven Anders <sven@anders.hamburg>
2016-11-15 13:31:03 +0100

    correct url

--- js/tempo30/view/layer/t30tmsurl.js
+++ js/tempo30/view/layer/t30tmsurl.js
@@ -3,7 +3,7 @@ define('tempo30/view/layer/t30tmsurl', [
 
     'use strict';
     
-    return "https://{s}.tools.adfc-hamburg.de/cgi-bin/tiles/t30/{layers}/{z}/{x}/{y}";
+    return "https://{s}.tools.adfc-hamburg.de/tiles/t30/{layers}/{z}/{x}/{y}";
     
     //return "https://{s}.tools.adfc-hamburg.de/cgi-bin/mapserv.fcgi?map=/opt/mapserver/maps/t30/t30.map&layers={layers}&mode=tile&tilemode=gmap&tile={x}+{y}+{z}";
 });
