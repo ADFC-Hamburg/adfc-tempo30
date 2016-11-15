ca939cf2fc792f64191612f73a939f7faf3e0340
Sven Anders <sven@anders.hamburg>
2016-11-15 13:03:45 +0100

    #64 auf fastcgi umgestellt

--- js/tempo30/view/layer/laerm_tag.js
+++ js/tempo30/view/layer/laerm_tag.js
@@ -1,19 +1,13 @@
 define('tempo30/view/layer/laerm_tag', [
     'leaflet',
-    'tempo30/view/layer/t30mapurl',
+    'tempo30/view/layer/t30tmsurl',
 ], function (L, mapurl) {
 
     'use strict';
     
-    //var layer = L.tileLayer.wms("http://geodienste.hamburg.de/HH_WMS_Strassenverkehr?zufall=0.21", {
-
-    var layer = L.tileLayer.wms(mapurl, {
+    var layer = L.tileLayer(mapurl, {
 	layers: 'Laerm_2012_Tag',
-	format: 'image/png',
-	crs: L.CRS.EPS4326,
-	transparent: true,
 	attribution: "Laermkarten Hamburg © Datenlizenz Deutschland Namensnennung 2.0"
     });
-    layer.options.crs = L.CRS.EPSG4326;
     return layer;
 });
